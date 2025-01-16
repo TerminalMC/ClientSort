7ddd027d75ef9a423bce940c628b55dabeed622a
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-01-16 16:40:03 +0800

    2025

--- common/src/main/java/dev/terminalmc/clientsort/inventory/sort/SortContext.java
+++ common/src/main/java/dev/terminalmc/clientsort/inventory/sort/SortContext.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2022 Siphalor
- * Copyright 2024 TerminalMC
+ * Copyright 2025 TerminalMC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
