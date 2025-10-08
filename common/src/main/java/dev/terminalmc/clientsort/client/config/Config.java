2aacea9821c953d2bc4e3c7bf72cce25b52c2b27
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-10-08 11:41:38 +0800

    Update to mc1.21.5

--- common/src/main/java/dev/terminalmc/clientsort/client/config/Config.java
+++ common/src/main/java/dev/terminalmc/clientsort/client/config/Config.java
@@ -336,7 +336,7 @@ public class Config {
                         Policy.KEYBIND_BUTTON,
                         null,
                         false,
-                        new TreeSet<>(List.of(0))
+                        new TreeSet<>()
                 ),
                 new ClassPolicy(
                         PlayerEnderChestContainer.class.getName(),
